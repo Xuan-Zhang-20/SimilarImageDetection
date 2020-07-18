13598b7f88c57f1af397f741475c9e421e4f0fe5
Xuan-Zhang-20 <zhang.x.bd@m.titech.ac.jp>
2020-07-19 01:09:54 +0800

    Get picture path

--- app/src/main/java/com/example/similarimagedetection/MainActivity.java
+++ app/src/main/java/com/example/similarimagedetection/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.similarimagedetection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public List<String> picPaths= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getPicList(View v) {
+        List<String> folders= new ArrayList<>();
+        folders.add(Environment.DIRECTORY_DCIM);
+        folders.add(Environment.DIRECTORY_DOWNLOADS);
+        folders.add(Environment.DIRECTORY_PICTURES);
+        String[] folderArray= (String[]) folders.toArray();
+        for (int i=0;i<folders.size();++i) {
+            File file=new File(folderArray[i]);
+            File[] files=file.listFiles();
+            if (files == null) {
+                Log.e("error", "空目录");
+                break;
+            }
+            for(int j=0;j<files.length;++j) {
+                if(files[i].getAbsolutePath().endsWith(".jpg") ||
+                        files[i].getAbsolutePath().endsWith(".png") ||
+                        files[i].getAbsolutePath().endsWith(".bmp") ||
+                        files[i].getAbsolutePath().endsWith(".jpeg"))
+                    picPaths.add(files[i].getAbsolutePath());
+            }
+        }
     }
 }
